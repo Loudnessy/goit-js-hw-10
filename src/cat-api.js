a01703957ae70f37733887df1150587aa741ff93
Loudnessy <eldarnaumenko2006@gmail.com>
2023-07-18 15:27:21 +0300

    homework 10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,26 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_XXlm7tWMwAcyP2o8jsdCaP8wnMrZ1UkTXmAUkCQOdXQ1drSXjtVI1dsCAB2Rgi9N";
+const options = {
+    headers: {
+        'x-api-key': 'live_XXlm7tWMwAcyP2o8jsdCaP8wnMrZ1UkTXmAUkCQOdXQ1drSXjtVI1dsCAB2Rgi9N' 
+    }
+
+}
 export const fetchBreeds = function () {
-    return fetch("https://api.thecatapi.com/v1/breeds")
+    return fetch("https://api.thecatapi.com/v1/breeds", options)
+    .then(resp => {
+        if(!resp.ok){
+            throw new Error(resp.statusText)
+        }
+        return resp.json()
+    })
 }
 export const fetchCatByBreed = function (breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, options)
+    .then(resp => {
+        if(!resp.ok){
+            throw new Error(resp.statusText)
+        }
+        return resp.json()
+    })
 }
 
 
--- src/index.js
+++ src/index.js
@@ -5,57 +5,55 @@ const div = document.querySelector(".cat-info")
 const paragraphLoading = document.querySelector(".loader")
 const paragraphError = document.querySelector(".error")
 fetchBreeds()
-.then(resp => {
-    if(!resp.ok){
-        throw new Error(resp.statusText)
-    }
-    return resp.json()
-})
 .then(data => {
     // console.log(data);
     select.innerHTML = data.map(elem => `<option value="${elem.id}">${elem.name}</option>`).join("");
 })
-.catch(() => paragraphError.removeAttribute('hidden'))
+.catch(() => {
+    paragraphError.removeAttribute('hidden')    
+    select.setAttribute("hidden", true)
+    div.setAttribute("hidden", true)
+   }
+  )
 .finally(() => paragraphLoading.setAttribute("hidden", true))
 
 select.addEventListener("change", onChange)
+
+
+
+
+
 function onChange (evt) {
+            paragraphLoading.removeAttribute("hidden")
     fetchCatByBreed(evt.target.value)
-    .then(resp => {
-        if(!resp.ok){
-            throw new Error(resp.statusText)
-        }
-        paragraphLoading.removeAttribute('hidden')
-        return resp.json()
-    })
     .then(data => {
+        if (data.length === 0) {
+        throw new Error(resp.statusText)
+        }
         const img = data.map(elem => 
     `<img src="${elem.url}" alt="cat" width="400" height="400">`
 ).join("")
 div.innerHTML = img
+data.map(elem => {
+    elem.breeds.forEach(cat => {
+  const array = [cat]
+        const findCatById = array.find(option => option.id === `${evt.target.value}`)
+        const markup = `<div class="flex">
+        <h2>${findCatById.name}</h2>
+        <p>${findCatById.description}</p>
+        <h2>Temperament</h2>
+        <p>${findCatById.temperament}</p>
+        </div>` 
+            div.insertAdjacentHTML("beforeend", markup) 
+    });
+})
     })
-    .catch(() => paragraphError.removeAttribute('hidden'))
-    fetchBreeds()
-    .then(resp => {
-        if(!resp.ok){
-            throw new Error(resp.statusText)
-        }
-        return resp.json()
-    })
-    .then(data => {
-  
-        const findCatById = data.find(option => option.id === `${evt.target.value}`)
-const markup = `<div class="flex">
-<h2>${findCatById.name}</h2>
-<p>${findCatById.description}</p>
-<h2>Temperament</h2>
-<p>${findCatById.temperament}</p>
-</div>` 
-
-
-    div.insertAdjacentHTML("beforeend", markup) 
-    })
-    .catch(() => paragraphError.removeAttribute('hidden'))
+    .catch(() => {
+     paragraphError.removeAttribute('hidden')    
+     select.setAttribute("hidden", true) 
+div.style.display = 'none'
+    }
+   )
     .finally(() => paragraphLoading.setAttribute("hidden", true))
 }
 
